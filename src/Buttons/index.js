1e0a492210be6f73f32842e0c96c62337a363490
Grzegorz Gołos <golosgit@gmail.com>
2022-07-11 16:00:25 +0200

    deleted unneccessary span elements

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,14 +3,10 @@ import "./style.css";
 const Buttons = ({ taskList, visibilityOfFinishedTasks }) =>
   taskList.length > 0 && (
     <>
-      <span className="buttons__gridElement">
         <button className="buttons__button">{visibilityOfFinishedTasks ? "Ukryj" : "Pokaż"} ukończone</button>
-      </span>
-      <span className="buttons__gridElement">
         <button className="buttons__button" disabled={taskList.every(({ status }) => status)}>
           Ukończ wszystkie
         </button>
-      </span>
     </>
   );
 
