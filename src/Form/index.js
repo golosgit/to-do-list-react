da91c477221cde979696987d026a12fcd2597b27
Grzegorz Gołos <golosgit@gmail.com>
2022-08-05 17:46:59 +0200

    added focus on input after adding new task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FormBody, Input, Button } from "./styled.js";
 
 export const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
+    inputRef.current.focus();
   };
 
   return (
     <FormBody onSubmit={onFormSubmit}>
       <Input
+        ref={inputRef}
         value={newTaskContent}
         onChange={({ target }) => setNewTaskContent(target.value)}
         placeholder="Co jest do zrobienia?"
