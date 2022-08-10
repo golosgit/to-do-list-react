41d505a88c86d742a271d22cd9585dd4dac5d24d
golosgit <golosgit@gmail.com>
2022-08-10 08:09:08 +0200

    added useTasks custom hook

--- src/App.js
+++ src/App.js
@@ -1,65 +1,26 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Form } from "./Form";
 import { Tasks } from "./Tasks";
 import { Buttons } from "./Buttons";
 import { Section } from "./Section";
 import { Header } from "./Header";
+import { useTasks } from "./useTasks.js";
 
 function App() {
-  const [taskList, setList] = useState(
-    JSON.parse(localStorage.getItem("taskList")) ? 
-    JSON.parse(localStorage.getItem("taskList")) : []
-  );
-
-  useEffect(() => {
-    localStorage.setItem("taskList", JSON.stringify(taskList));
-  }, [taskList]);
-
   const [visibilityOfFinishedTasks, setVisibility] = useState(true);
-
-  const toggleTaskDone = (id) => {
-    setList((taskList) =>
-      taskList.map((task) => {
-        if (task.id === id) {
-          return { ...task, status: !task.status };
-        }
-        return task;
-      })
-    );
-  };
-
-  const removeTask = (id) => {
-    setList((taskList) => taskList.filter((task) => task.id !== id));
-  };
 
   const toggleVisibility = () => {
     setVisibility((visibilityOfFinishedTasks) => !visibilityOfFinishedTasks);
   };
 
-  const finishAllTasks = () => {
-    setList((taskList) => taskList.map((task) => ({ ...task, status: true })));
-  };
-
-  const addNewTask = (newTaskContent) => {
-    if (newTaskContent) {
-      setList((taskList) => [
-        ...taskList,
-        {
-          id: taskList.length ? taskList[taskList.length - 1].id + 1 : 1,
-          content: newTaskContent,
-          status: false,
-        },
-      ]);
-    }
-  };
+  const { taskList, toggleTaskDone, removeTask, finishAllTasks, addNewTask } = useTasks();
 
   return (
     <>
       <Header title="Lista zadań" />
       <Section 
         title="Dodaj nowe zadanie" 
-        extraSectionContent={<Form addNewTask={addNewTask} />} 
-      />
+        extraSectionContent={<Form addNewTask={addNewTask} />} />
       <Section
         listSection
         title="Lista zadań"
