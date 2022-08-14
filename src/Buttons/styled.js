868f40d7e173ae78c9f14d4ae4115905fba1b1d4
golosgit <golosgit@gmail.com>
2022-08-14 11:37:12 +0200

    updated code formatting

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -6,13 +6,16 @@ export const Button = styled.button`
   border: 2px solid transparent;
   font-size: medium;
   transition: color 0.3s;
+
   &:hover {
     color: hsl(180, 100%, 33%);
   }
+
   &:active {
     border: 2px solid #333;
     border-radius: 2px;
   }
+
   &:disabled {
     color: #bbb;
     border: 2px solid transparent;
