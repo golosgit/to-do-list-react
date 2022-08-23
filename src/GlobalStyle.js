6b5080f2d925ad3e3e1cdfa4bf2118ccc039654e
golosgit <golosgit@gmail.com>
2022-08-23 19:56:38 +0200

    implemented GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    background-color: #e1e1e1;
+    line-height: 1.5;
+    font-family: "Montserrat", sans-serif;
+    margin: 20px auto;
+    max-width: 950px;
+    overflow-wrap: anywhere;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
