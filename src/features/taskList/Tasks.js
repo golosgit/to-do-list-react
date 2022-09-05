c91b7e340710ee6537cf853c949359de22257344
golosgit <golosgit@gmail.com>
2022-09-05 19:18:43 +0200

    deleted unnecessary props

--- src/features/taskList/Tasks.js
+++ src/features/taskList/Tasks.js
@@ -1,48 +1,25 @@
-import { useState } from "react";
 import { Form } from "./Form";
 import { TaskList } from "./TaskList";
 import { Buttons } from "./Buttons";
 import { Section } from "../../common/Section";
 import { Header } from "../../common/Header";
-import { useTasks } from "../../useTasks";
 
 function Tasks() {
-  const [visibilityOfFinishedTasks, setVisibility] = useState(true);
-
-  const toggleVisibility = () => {
-    setVisibility((visibilityOfFinishedTasks) => !visibilityOfFinishedTasks);
-  };
-
-  const { taskList, toggleTaskDone, removeTask, finishAllTasks, addNewTask } = useTasks();
-
   return (
     <>
       <Header title="Lista zadań" />
-      <Section 
-        title="Dodaj nowe zadanie" 
-        extraSectionContent={<Form addNewTask={addNewTask} />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        extraSectionContent={<Form />}
+      />
       <Section
         listSection
         title="Lista zadań"
-        extraSectionButtons={
-          <Buttons
-            taskList={taskList}
-            visibilityOfFinishedTasks={visibilityOfFinishedTasks}
-            toggleVisibility={toggleVisibility}
-            finishAllTasks={finishAllTasks}
-          />
-        }
-        extraSectionContent={
-          <TaskList
-            toggleTaskDone={toggleTaskDone}
-            removeTask={removeTask}
-            taskList={taskList}
-            visibilityOfFinishedTasks={visibilityOfFinishedTasks}
-          />
-        }
+        extraSectionButtons={<Buttons />}
+        extraSectionContent={<TaskList />}
       />
     </>
-  );
+  )
 }
 
 export default Tasks;
