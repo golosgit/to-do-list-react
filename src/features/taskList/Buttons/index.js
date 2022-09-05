083d73ad7181ce3aa1a8d92c5dcea6f758e2681c
golosgit <golosgit@gmail.com>
2022-09-05 19:18:03 +0200

    toggleVisibility and finishAllTasks dispatch added

--- src/features/taskList/Buttons/index.js
+++ src/features/taskList/Buttons/index.js
@@ -1,18 +1,27 @@
 import { Button } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleVisibility, finishAllTasks } from "../taskListSlice";
 
-export const Buttons = ({ taskList, visibilityOfFinishedTasks, toggleVisibility, finishAllTasks }) =>
-  taskList.length > 0 && (
-    <>
-      <Button 
-        onClick={toggleVisibility}
-      >
-        {visibilityOfFinishedTasks ? "Ukryj" : "Pokaż"}&nbsp;ukończone
-      </Button>
-      <Button 
-        onClick={finishAllTasks} 
-        disabled={taskList.every(({ status }) => status)}
-      >
-        Ukończ wszystkie
-      </Button>
-    </>
-  );
+export const Buttons = () => {
+  const dispatch = useDispatch();
+  const { taskList, visibilityOfFinishedTasks } = useSelector(selectTasks);
+  
+  return (
+    taskList.length > 0 && (
+      <>
+        <Button
+          onClick={() => dispatch(toggleVisibility())}
+        >
+          {visibilityOfFinishedTasks ? "Ukryj" : "Pokaż"}&nbsp;ukończone
+        </Button>
+        <Button
+          onClick={() => dispatch(finishAllTasks())}
+          disabled={taskList.every(({ status }) => status)}
+        >
+          Ukończ wszystkie
+        </Button>
+      </>
+    )
+  )
+};
+
