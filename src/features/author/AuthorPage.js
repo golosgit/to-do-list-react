107838da96ca0159fb529a7f9f4a05c0f4cab1c3
golosgit <107067096+golosgit@users.noreply.github.com>
2022-09-19 20:47:20 +0200

    deleted unnecessary React Fragment

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -9,20 +9,18 @@ export const Author = () => {
       <Section
         title="Grzegorz Gołos"
         sectionContent={
-          <>
-            <SectionContentWrapper>
-              <p>
-                Zawsze miałem problemy z pisaniem tekstów, zatem będzie krótko. Jestem Grzesiek i obecnie pracuję jako
-                metrolog w firmie produkującej zewnętrzne elementy karoserii samochodowych. Od około roku zastanawiam
-                się nad zmianą branży, ale dopiero parę miesięcy temu podjąłem decyzję o spróbowaniu się we Frontendzie.
-              </p>
-              <p>
-                W wolnych chwilach lubię słuchać muzyki (w szczególności ambient i jego pochodne). Pożeraczem czasu są
-                również gry single player (w tym wypadku prym wiodą RPG-i), chociaż ostatnio do tego typu rozrywki
-                ciągnie mnie coraz mniej.
-              </p>
-            </SectionContentWrapper>
-          </>
+          <SectionContentWrapper>
+            <p>
+              Zawsze miałem problemy z pisaniem tekstów, zatem będzie krótko. Jestem Grzesiek i obecnie pracuję jako
+              metrolog w firmie produkującej zewnętrzne elementy karoserii samochodowych. Od około roku zastanawiam się
+              nad zmianą branży, ale dopiero parę miesięcy temu podjąłem decyzję o spróbowaniu się we Frontendzie.
+            </p>
+            <p>
+              W wolnych chwilach lubię słuchać muzyki (w szczególności ambient i jego pochodne). Pożeraczem czasu są
+              również gry single player (w tym wypadku prym wiodą RPG-i), chociaż ostatnio do tego typu rozrywki ciągnie
+              mnie coraz mniej.
+            </p>
+          </SectionContentWrapper>
         }
       />
     </Wrapper>
